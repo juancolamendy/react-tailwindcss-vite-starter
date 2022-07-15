b433d2063bb7a4079677fdf945b8de96d2660d87
JC Olamendy <juanc.olamendy@gmail.com>
2022-07-15 17:23:14 -0400

    initial react vite structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+
+import Routes from "./Routes";
+
+import './styles/main.css';
+
+const App = () => {
+  const pages = import.meta.globEager("./pages/**/!(*.test.[jt]sx)*.([jt]sx)");
+
+  return (
+    <BrowserRouter>
+      <Routes pages={pages} />
+    </BrowserRouter>
+  );
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import "dotenv/config";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // server config
+  server: {
+    port: 3000,
+  },
+  
+  // plugins
+  plugins: [react()]
+});
